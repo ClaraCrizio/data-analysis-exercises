82d1ed9bf9411337b458087e7908b17410427d36
Clara Crizio <91985269+ClaraCrizio@users.noreply.github.com>
2023-04-22 19:09:03 -0300

    Update SimpleQuestions.R

--- SimpleQuestions.R
+++ SimpleQuestions.R
@@ -18,16 +18,49 @@ describe(Checkouts_By_Title_Data_Lens_2017) # Checkouts
 describe(Library_Collection_Inventory) # Inventário: 2.687.149 registros
 
 # Tratamento dos dados removendo registros duplicados do inventário - critério: última data de report de localização:
-# Os dados da coleção (Library_Collection_Iventory) são duplicados para atualizar a localização dos itens na biblioteca.
 
-Library_Collection_Inventory <- Library_Collection_Inventory %>% 
+# Identificando registros duplicados:
+Inventory_Duplicates <- Library_Collection_Inventory %>% 
+  add_count(BibNum) %>%
+  filter(n > 1)
+  
+# Identificando número de registros distintos, considerando a última ReportDate:
+Inventory_Distinct <- Library_Collection_Inventory %>% 
   arrange(desc(ReportDate)) %>% 
   distinct(BibNum, .keep_all = TRUE)
 
 # Restam 584.391 registros únicos (BibNum)
 
+# Incluir última contagem dos itens de acordo com a ReportDate mais recente:
+# Foram removidas colunas de localização/coleção que não agregam informação para a tabela distinta
+
+Collection_Inventory_ItemCount <- Library_Collection_Inventory %>%  
+  group_by(BibNum, ReportDate) %>% 
+  summarise(Total_ItemCount=sum(ItemCount)) %>% 
+  arrange(desc(ReportDate)) %>% 
+  distinct(BibNum, .keep_all = TRUE)
+
+Inventory_Distinct_Final <- Inventory_Distinct %>% 
+  left_join(Collection_Inventory_ItemCount, by = c("BibNum", "ReportDate")) %>% 
+  select(-c(ItemCollection, ItemLocation, FloatingItem, ItemCount))
+
+#' Explicação dos tratamentos realizados:
+#' Os dados da coleção (Library_Collection_Iventory) são duplicados para atualizar a localização dos itens na biblioteca.
+#' 
+#' Foram realizados testes para compreender a dinâmica de ReportDate dos itens e identifiquei que existem itens com último 
+#' ReportDate em 09/2017 e que tiveram checkouts registrados em data posterior no mesmo ano, o que sugere que nem todos 
+#' os itens do inventário foram reportados na última contagem. Por isso, ao limpar a base de dados, foram considerados como 
+#' itens presentes no inventário todos os registros de BibNum, mesmo os que não tiveram ReportDate em 10/2017 
+#' (estes foram ao todo 199 registros). Caso se compreenda que devem ser considerados apenas os itens do último Report, basta 
+#' filtrar pelo campo ReportDate a data mais recente, que apresentará apenas os itens da contagem mais atual.
+#' 
+#' Desta forma, para a contagem do total de itens, foram considerados duplicados apenas os itens que possuem todos os campos iguais
+#' exceto ReportDate (nesse caso, foram considerados apenas os mais atuais), pois entende-se que podem se tratar de itens de 
+#' coleções diferentes, e/ou que estavam em localizações diferentes na mesma data, e que, portanto, devem ser contabilizados
+#' como itens diferentes do acervo.
+
 #-------------------------------------------
-# How many books were checkout in 2017?
+# How many books were checkedout in 2017?
 
 Checkout_Books_2017 <- Checkouts_By_Title_Data_Lens_2017 %>% 
   left_join(Integrated_Library_System__ILS__Data_Dictionary, by = c("ItemType"="Code")) %>% 
@@ -49,18 +82,17 @@ Code_Books <- Integrated_Library_System__ILS__Data_Dictionary %>%
   mutate(Books = 1)
 
 # Calculando total de livros:
-Total_Books <- Library_Collection_Inventory %>% 
+Total_Books <- Inventory_Distinct_Final %>% 
   left_join(Code_Books, by = c("ItemType" = "Code")) %>% 
-  summarise(N_Books = sum(ItemCount*Books, na.rm = TRUE)) %>% 
-  summarise(Total_Books = sum(N_Books))
+  summarise(N_Books = sum(Total_ItemCount*Books, na.rm = TRUE))
 
-# Resposta: Total de livros = 620.819
+# Resposta: Total de livros = 1.332.993
 
 #--------------------------------------------
 # How many titles?
 
 # Número de títulos de livros:
-Total_Book_Titles <- Library_Collection_Inventory %>%
+Total_Book_Titles <- Inventory_Distinct_Final %>%
   left_join(Code_Books, by = c("ItemType" = "Code")) %>% 
   filter(Books==1) %>% 
   summarise(N_Titles=n_distinct(BibNum))
@@ -68,7 +100,7 @@ Total_Book_Titles <- Library_Collection_Inventory %>%
 # Resposta: Total de Títulos (livros) =	497.692
 
 # Número de títulos (todos itens):
-Total_Titles <- Library_Collection_Inventory %>%
+Total_Titles <- Inventory_Distinct_Final %>%
   summarise(N_Titles=n_distinct(BibNum))
 
 # Resposta: Total de Títulos (todos itens) = 584.391
@@ -79,7 +111,7 @@ Top10_Books <- Checkout_Books_2017 %>%
   group_by(BibNumber) %>% 
   summarise(N_Checkouts=n()) %>% 
   slice_max(N_Checkouts, n=10) %>% 
-  left_join(Library_Collection_Inventory, by = c('BibNumber'='BibNum')) %>% 
+  left_join(Inventory_Distinct_Final, by = c('BibNumber'='BibNum')) %>% 
   select(BibNumber, N_Checkouts, Title) %>% 
   distinct()
 
